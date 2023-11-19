99ba4b46dce7103385b17983f7b44c215c4cf491
Andreastephgm <98714194+Andreastephgm@users.noreply.github.com>
2023-11-19 00:46:05 -0500

    commitHome
    Changes in home.

---
+++ AnimeFlix/scripts/main.js
@@ -0,0 +1,13 @@
+const row = document.querySelector('series')
+const movie = document.querySelector('movies');
+
+const rightArrow = document.getElementsById('buttonRight');
+const leftArrow = document.getElementsById('buttonLeft');
+
+/*rightArrow.addEventListener('click', () => {
+    row.scrollLeft +=  row.offsetWidth;
+});
+
+leftArrow.addEventListener('click', () => {
+    row.scrollLeft -=  row.offsetWidth;
+});*/
