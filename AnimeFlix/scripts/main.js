c5170878d64159d0f76e93edd4a097d4bba63588
Andreastephgm <98714194+Andreastephgm@users.noreply.github.com>
2023-12-28 13:14:09 -0500

    commit changes CSS
    Changes index, main and styles

--- AnimeFlix/scripts/main.js
+++ AnimeFlix/scripts/main.js
@@ -1,6 +1,6 @@
 //PRINCIPAL CONTAINER
 const series = document.querySelector('.series');
-const movie = document.querySelectorAll('.movies');
+const movie = document.querySelectorAll('.popular');
 
 const buttonRight = document.getElementById('buttonRight');
 const buttonLeft = document.getElementById('buttonLeft');
@@ -14,7 +14,7 @@ buttonLeft.addEventListener('click', () => {
 });
 //ACTION CONTAINER
 const series1 = document.querySelector('#series1');
-const movie1 = document.querySelectorAll('#movies1');
+const movie1 = document.querySelectorAll('.popular');
 
 const buttonRight1 = document.getElementById('buttonRight1');
 const buttonLeft1 = document.getElementById('buttonLeft1');
@@ -29,7 +29,7 @@ buttonLeft1.addEventListener('click', () => {
 
 //PARANORMAL CONTAINER
 const series2 = document.querySelector('#series2');
-const movie2 = document.querySelectorAll('#movies2');
+const movie2 = document.querySelectorAll('.popular');
 
 const buttonRight2 = document.getElementById('buttonRight2');
 const buttonLeft2 = document.getElementById('buttonLeft2');
@@ -44,7 +44,7 @@ buttonLeft2.addEventListener('click', () => {
 
 //HIGH SCHOOL CONTAINER
 const series3 = document.querySelector('#series3');
-const movie3 = document.querySelectorAll('#movies3');
+const movie3 = document.querySelectorAll('.popular');
 
 const buttonRight3 = document.getElementById('buttonRight3');
 const buttonLeft3 = document.getElementById('buttonLeft3');
@@ -59,7 +59,7 @@ buttonLeft3.addEventListener('click', () => {
 
 //CLASSICS CONTAINER
 const series4 = document.querySelector('#series4');
-const movie4 = document.querySelectorAll('#movies4');
+const movie4 = document.querySelectorAll('.popular');
 
 const buttonRight4 = document.getElementById('buttonRight4');
 const buttonLeft4 = document.getElementById('buttonLeft4');
@@ -74,7 +74,7 @@ buttonLeft4.addEventListener('click', () => {
 
 //KIDS CONTAINER
 const series5 = document.querySelector('#series5');
-const movie5 = document.querySelectorAll('#movies5');
+const movie5 = document.querySelectorAll('.popular');
 
 const buttonRight5 = document.getElementById('buttonRight5');
 const buttonLeft5 = document.getElementById('buttonLeft5');
@@ -86,3 +86,14 @@ buttonRight5.addEventListener('click', () => {
 buttonLeft5.addEventListener('click', () => {
     series5.scrollLeft -=  series5.offsetWidth;
 });
+
+//POPULAR INDICATORS
+const pageNumber = Math.ceil(movie.length/5);
+for (let i = 0; i < pageNumber; i++) {
+    const indicator = document.createElement('button');
+    if(i===0){
+        indicator.classList.add('active');
+    }
+    document.querySelector('.popularIndicator').appendChild(indicator);
+}
+
